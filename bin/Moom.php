7fbb28dd916bd7f2624e96bd14e7c0469bd0c9a2
PaisleyMeadow <paisley1000@gmail.com>
2020-12-04 19:03:36 -0700

    image check (again)

--- bin/Moom.php
+++ bin/Moom.php
@@ -1,10 +1,5 @@
-<?php $_SESSION['currspace'] = 'Moom';?><div class="workspace-container"><div class="workspace-border-container" style="background-color:#323478;"><div class="workspace" id="Moom" style="background-color:#323478;"><div><img class="draggable" data-id="491" src="images/shipBeige.png"></div>
-<div><img class="draggable" data-id="501" src="bin/images/shipBeige_damage1.png"></div>
-<div><img class="draggable" data-id="511" src="uploaded/shipBeige.png"></div>
-<div><img class="draggable" data-id="521" src="uploaded/shipBeige.png"></div>
-<div><img class="draggable" data-id="531" src="uploaded/laserBlue3.png"></div>
-<div><img class="draggable" data-id="541" src="uploaded/pen-outline-edit-interface-symbol-pngrepo-com (1).png"></div>
-<div><img class="draggable" data-id="551" src="uploaded/envelope-1.png (3).png"></div>
+<?php $_SESSION['currspace'] = 'Moom';?><div class="workspace-container"><div class="workspace-border-container" style="background-color:#323478;"><div class="workspace" id="Moom" style="background-color:#323478;"><div><img class="draggable" data-id="581" src="bin/imagesgithub.png"></div>
+<div><img class="draggable" data-id="601" src="bin/imagesnyan-cat.gif"></div>
 <textarea class="ubuntu-font draggable" data-id="161" style="background-color:fdf0d5;"></textarea>
 <textarea class="ubuntu-font draggable" data-id="171" style="background-color:2791BC;"></textarea>
 </div></div></div>
