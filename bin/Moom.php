960f89ed9c256e1efb3d8dd387162f01905a3435
PaisleyMeadow <paisley1000@gmail.com>
2020-12-04 19:28:59 -0700

    1

--- bin/Moom.php
+++ bin/Moom.php
@@ -1,5 +1,3 @@
-<?php $_SESSION['currspace'] = 'Moom';?><div class="workspace-container"><div class="workspace-border-container" style="background-color:#323478;"><div class="workspace" id="Moom" style="background-color:#323478;"><div><img class="draggable" data-id="581" src="bin/imagesgithub.png"></div>
-<div><img class="draggable" data-id="601" src="bin/imagesnyan-cat.gif"></div>
-<textarea class="ubuntu-font draggable" data-id="161" style="background-color:fdf0d5;"></textarea>
+<?php $_SESSION['currspace'] = 'Moom';?><div class="workspace-container"><div class="workspace-border-container" style="background-color:#323478;"><div class="workspace" id="Moom" style="background-color:#323478;"><textarea class="ubuntu-font draggable" data-id="161" style="background-color:fdf0d5;"></textarea>
 <textarea class="ubuntu-font draggable" data-id="171" style="background-color:2791BC;"></textarea>
 </div></div></div>
